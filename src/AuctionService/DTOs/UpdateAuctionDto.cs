54da6b9c0ecba2e9951a674f4c85aa3e71f8c873
Yan Aung <yaung@servicetitan.com>
2024-07-25 19:54:24 -0700

    Add controllers for Create/Update/Delete auctions

--- src/AuctionService/DTOs/UpdateAuctionDto.cs
+++ src/AuctionService/DTOs/UpdateAuctionDto.cs
@@ -6,10 +6,10 @@ public class UpdateAuctionDto
 
     public string Model { get; set; }
 
-    public int Year { get; set; }
+    public int? Year { get; set; }
 
     public string Color { get; set; }
 
-    public int Mileage { get; set; }
+    public int? Mileage { get; set; }
 
 }
