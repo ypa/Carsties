021510a52227c0de0c59d5e35f12e482194dae41
Yan Aung <yaung@servicetitan.com>
2024-08-01 19:27:08 -0700

    Fecth and load data by calling AuctionService

--- src/SearchService/Data/DbInitializer.cs
+++ src/SearchService/Data/DbInitializer.cs
@@ -20,17 +20,15 @@ public class DbInitializer
 
         var count = await DB.CountAsync<Item>();
 
-        if (count == 0)
-        {
-            Console.WriteLine("No data - will attempt to seed");
-            var itemData = await File.ReadAllTextAsync("Data/auctions.json");
+        using var scope = app.Services.CreateScope();
 
-            var options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
+        var httpClient = scope.ServiceProvider.GetRequiredService<AuctionSvcHttpClient>();
 
-            var items = JsonSerializer.Deserialize<List<Item>>(itemData, options);
+        var items = await httpClient.GetItemsForSearchDb();
 
-            await DB.SaveAsync(items);
-        }
+        Console.WriteLine(items.Count + " returned from the auction service");
+
+        if (items.Count > 0) await DB.SaveAsync(items);
 
     }
 }
---
+++ src/SearchService/Services/AuctionSvcHttpClient.cs
@@ -0,0 +1,26 @@
+﻿using MongoDB.Entities;
+
+namespace SearchService;
+
+public class AuctionSvcHttpClient
+{
+    private readonly IConfiguration _config;
+    private readonly HttpClient _httpClient;
+    public AuctionSvcHttpClient(HttpClient httpClient, IConfiguration config)
+    {
+        _httpClient = httpClient;
+        _config = config;
+    }
+
+    public async Task<List<Item>> GetItemsForSearchDb()
+    {
+        var lastUpdated = await DB.Find<Item, string>()
+            .Sort(x => x.Descending(x => x.UpdateAt))
+            .Project(x => x.UpdateAt.ToString())
+            .ExecuteFirstAsync();
+
+        return await _httpClient.GetFromJsonAsync<List<Item>>(_config["AuctionServiceUrl"]
+            + "/api/auctions?date=" + lastUpdated);
+    }
+
+}
