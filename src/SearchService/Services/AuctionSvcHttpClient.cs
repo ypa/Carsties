3da71c5a87432e730511fec35f042cf50833e8ca
Yan Aung <yaung@servicetitan.com>
2024-08-02 20:25:53 -0700

    Rename field to UpdatedAt

--- src/SearchService/Services/AuctionSvcHttpClient.cs
+++ src/SearchService/Services/AuctionSvcHttpClient.cs
@@ -15,8 +15,8 @@ public class AuctionSvcHttpClient
     public async Task<List<Item>> GetItemsForSearchDb()
     {
         var lastUpdated = await DB.Find<Item, string>()
-            .Sort(x => x.Descending(x => x.UpdateAt))
-            .Project(x => x.UpdateAt.ToString())
+            .Sort(x => x.Descending(x => x.UpdatedAt))
+            .Project(x => x.UpdatedAt.ToString())
             .ExecuteFirstAsync();
 
         return await _httpClient.GetFromJsonAsync<List<Item>>(_config["AuctionServiceUrl"]
