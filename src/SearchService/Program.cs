c1ea7142b88e9b055251a8830eb9b92dd89f048f
Yan Aung <yaung@servicetitan.com>
2024-08-05 19:56:55 -0700

    Retry consuming Rabbit messages in the event that mongodb is down

--- src/SearchService/Program.cs
+++ src/SearchService/Program.cs
@@ -19,6 +19,13 @@ builder.Services.AddMassTransit(x =>
 
     x.UsingRabbitMq((context, cfg) =>
     {
+        cfg.ReceiveEndpoint("search-auction-created", endpoint =>
+        {
+            // Retry up to 5 times waiting 5 seconds in between
+            endpoint.UseMessageRetry(r => r.Interval(5, 5));
+            endpoint.ConfigureConsumer<AuctionCreatedConsumer>(context);
+        });
+
         cfg.ConfigureEndpoints(context);
     });
 });
