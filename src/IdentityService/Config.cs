64e81b1f94cb1d0fe0122e1f9cb38634fcc9eca5
Yan Aung <yaung@servicetitan.com>
2024-08-17 19:14:59 -0700

    Add Client config for Next App

--- src/IdentityService/Config.cs
+++ src/IdentityService/Config.cs
@@ -28,6 +28,20 @@ public static class Config
                 RedirectUris = {"https://www.getpostman.com/oauth2/callback"},
                 ClientSecrets = new[] {new Secret("NotASecret".Sha256())},
                 AllowedGrantTypes = {GrantType.ResourceOwnerPassword}
+            },
+            new Client
+            {
+                ClientId = "nextApp",
+                ClientName = "nextApp",
+                ClientSecrets = {new Secret("secret".Sha256())},
+                // OAuth handshake can happen behind the scene over the network, no broswer involvement needed
+                // For a mobile app client use GrantTypes.Code -- YP 08/17/2024
+                AllowedGrantTypes = GrantTypes.CodeAndClientCredentials,
+                RequirePkce = false, // For a mobile app client use true -- YP 08/17/2024
+                RedirectUris = {"http://localhost:3000/api/auth/callback/id-server"},
+                AllowOfflineAccess = true, // For refresh token functionality -- YP 08/17/2024
+                AllowedScopes = {"openid", "profile", "auctionApp"},
+                AccessTokenLifetime = 3600*24*30 // During development mode keep it 30 days for now, default is one hour -- YP 08/17/2024
             }
         };
 }
