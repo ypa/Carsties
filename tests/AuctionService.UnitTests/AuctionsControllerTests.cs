9ab0223b8d9715287ae154b6412c372432daa945
Yan Aung <yaung@servicetitan.com>
2024-08-25 18:54:47 -0700

    Add AuctionsControllerTests and setup with mock

---
+++ tests/AuctionService.UnitTests/AuctionsControllerTests.cs
@@ -0,0 +1,35 @@
+using System;
+using AuctionService.Controllers;
+using AuctionService.Data;
+using AuctionService.RequestHelpers;
+using AutoFixture;
+using AutoMapper;
+using MassTransit;
+using Moq;
+
+namespace AuctionService.UnitTests;
+
+public class AuctionsControllerTests
+{
+    private readonly Mock<IAuctionRepository> _auctionRepo;
+    private readonly Mock<IPublishEndpoint> _publishEndpoint;
+    private readonly Fixture _fixture;
+    private readonly AuctionsController _controller;
+    private readonly IMapper _mapper;
+
+    public AuctionsControllerTests()
+    {
+        _fixture = new Fixture();
+        _auctionRepo = new Mock<IAuctionRepository>();
+        _publishEndpoint = new Mock<IPublishEndpoint>();
+
+        var mockMapper = new MapperConfiguration(mc =>
+        {
+            mc.AddMaps(typeof(MappingProfiles).Assembly);
+        }).CreateMapper().ConfigurationProvider;
+
+        _mapper = new Mapper(mockMapper);
+        _controller = new AuctionsController(_auctionRepo.Object, _mapper, _publishEndpoint.Object);
+    }
+
+}
