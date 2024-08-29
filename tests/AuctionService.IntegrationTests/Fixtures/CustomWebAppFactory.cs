169e8da65f92a4ceb907b40a6d038454ff137b22
Yan Aung <yaung@servicetitan.com>
2024-08-28 19:30:04 -0700

    Setup CustomWebAppFactory to override AuctionService's Program class

--- tests/AuctionService.IntegrationTests/Fixtures/CustomWebAppFactory.cs
+++ tests/AuctionService.IntegrationTests/Fixtures/CustomWebAppFactory.cs
@@ -1,5 +1,12 @@
 using System;
+using AuctionService.Data;
+using MassTransit;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 using Testcontainers.PostgreSql;
 
 namespace AuctionService.IntegrationTests.Fixtures;
@@ -10,6 +17,36 @@ public class CustomWebAppFactory : WebApplicationFactory<Program>, IAsyncLifetim
     public async Task InitializeAsync()
     {
         await _postgreSqlContainer.StartAsync();
+    }
+
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        // Replace services in AuctionService's Program class for testing
+        builder.ConfigureTestServices(services =>
+        {
+            // replace DbContext
+            var descriptor = services.SingleOrDefault(d => d.ServiceType == typeof(DbContextOptions<AuctionDbContext>));
+
+            if (descriptor != null) services.Remove(descriptor);
+
+            services.AddDbContext<AuctionDbContext>(options =>
+            {
+                options.UseNpgsql(_postgreSqlContainer.GetConnectionString());
+            });
+
+            // replace RabbitMq
+            services.AddMassTransitTestHarness();
+
+            // migrate database
+            var sp = services.BuildServiceProvider();
+
+            using var scope = sp.CreateScope();
+            var scopedServices = scope.ServiceProvider;
+            var db = scopedServices.GetRequiredService<AuctionDbContext>();
+
+            db.Database.Migrate();
+
+        });
     }
 
     Task IAsyncLifetime.DisposeAsync() => _postgreSqlContainer.DisposeAsync().AsTask();
