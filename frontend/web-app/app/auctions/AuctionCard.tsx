b0b3a589e5acac3d87ec527e364929c9e6fff674
Yan Aung <yaung@servicetitan.com>
2024-09-05 19:40:26 -0700

    Add auction CountdownTimer on AuctionCard

--- frontend/web-app/app/auctions/AuctionCard.tsx
+++ frontend/web-app/app/auctions/AuctionCard.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import React from "react";
+import CountdownTimer from "./CountdownTimer";
 
 type Props = {
   auction: any;
@@ -8,7 +9,7 @@ type Props = {
 export default function AuctionCard({ auction }: Props) {
   return (
     <a href="#">
-      <div className="relative w-full bg-gray-200 aspect-video rounded-lg overflow-hidden">
+      <div className="relative w-full bg-gray-200 aspect-[16/10] rounded-lg overflow-hidden">
         <Image
           src={auction.imageUrl}
           alt={`Image of ${auction.make} ${auction.model} in ${auction.color}`}
@@ -17,6 +18,9 @@ export default function AuctionCard({ auction }: Props) {
           className="object-cover"
           sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 25vw"
         />
+        <div className="absolute bottom-2 left-2">
+          <CountdownTimer auctionEnd={auction.auctionEnd} />
+        </div>
       </div>
       <div className="flex justify-between items-center mt-4">
         <h3 className="text-gray-700">
