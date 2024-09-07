d1f7727b1294ecd6b8b56c73135aa80b50435592
Yan Aung <yaung@servicetitan.com>
2024-09-06 19:11:14 -0700

    Add Auction paginated types and refactor code to use these types

--- frontend/web-app/app/auctions/Listings.tsx
+++ frontend/web-app/app/auctions/Listings.tsx
@@ -1,7 +1,8 @@
 import React from "react";
 import AuctionCard from "./AuctionCard";
+import { Auction, PagedResult } from "@/types";
 
-async function getData() {
+async function getData(): Promise<PagedResult<Auction>> {
   const res = await fetch("http://localhost:6001/search?pageSize=10");
 
   if (!res.ok) throw new Error("Failed to fetch data");
@@ -15,7 +16,7 @@ export default async function Listings() {
   return (
     <div className="grid grid-cols-4 gap-6">
       {data &&
-        data.results.map((auction: any) => (
+        data.results.map((auction) => (
           <AuctionCard auction={auction} key={auction.id} />
         ))}
     </div>
