930dd21a0c91a9d3045159dcfbaad446671156f6
Yan Aung <yaung@servicetitan.com>
2024-09-08 18:39:46 -0700

    Add page size filters

--- frontend/web-app/app/actions/auctionActions.ts
+++ frontend/web-app/app/actions/auctionActions.ts
@@ -2,9 +2,12 @@
 
 import { Auction, PagedResult } from "@/types";
 
-export async function getData(pageNumber = 1): Promise<PagedResult<Auction>> {
+export async function getData(
+  pageNumber: number,
+  pageSize: number
+): Promise<PagedResult<Auction>> {
   const res = await fetch(
-    `http://localhost:6001/search?pageSize=4&pageNumber=${pageNumber}`
+    `http://localhost:6001/search?pageSize=${pageSize}&pageNumber=${pageNumber}`
   );
 
   if (!res.ok) throw new Error("Failed to fetch data");
