cb37d7b693cd66356493b4907f1901ce545d4c8e
Yan Aung <yaung@servicetitan.com>
2024-09-13 19:35:57 -0700

    Configure AuthJs for Duende IdentityServer

---
+++ frontend/web-app/auth.ts
@@ -0,0 +1,19 @@
+import NextAuth, { Profile } from "next-auth";
+import { OIDCConfig } from "next-auth/providers";
+import DuendeIDS6Provider from "next-auth/providers/duende-identity-server6";
+
+export const { handlers, signIn, signOut, auth } = NextAuth({
+  session: {
+    strategy: "jwt",
+  },
+  providers: [
+    DuendeIDS6Provider({
+      id: "id-server",
+      clientId: "nextApp",
+      clientSecret: "secret",
+      issuer: "http://localhost:5000",
+      authorization: { params: { scope: "openid profile auctionApp" } },
+      idToken: true,
+    } as OIDCConfig<Profile>),
+  ],
+});
