1e09adaa776a787f6e16504673f937f3d09b31b0
Yan Aung <yaung@servicetitan.com>
2024-09-15 19:21:51 -0700

    Protect /session route with authentication

--- frontend/web-app/auth.ts
+++ frontend/web-app/auth.ts
@@ -17,6 +17,9 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
     } as OIDCConfig<Omit<Profile, "username">>),
   ],
   callbacks: {
+    async authorized({ auth }) {
+      return !!auth;
+    },
     async jwt({ token, profile }) {
       if (profile) {
         token.username = profile.username;
