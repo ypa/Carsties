285730be3b5fc55bcbb0f8ea634453afe6dbe7e0
Yan Aung <yaung@servicetitan.com>
2024-09-18 18:54:40 -0700

    Show list auctions for user where s/he is winner and seller

--- frontend/web-app/app/nav/UserActions.tsx
+++ frontend/web-app/app/nav/UserActions.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { useParamsStore } from "@/hooks/useParamsStore";
 import {
   Button,
   Dropdown,
@@ -9,7 +10,7 @@ import {
 import { User } from "next-auth";
 import { signOut } from "next-auth/react";
 import Link from "next/link";
-import { useRouter } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
 import React from "react";
 import { AiFillCar, AiFillTrophy, AiOutlineLogout } from "react-icons/ai";
 import { HiCog, HiUser } from "react-icons/hi";
@@ -20,14 +21,28 @@ type Props = {
 
 export default function UserActions({ user }: Props) {
   const router = useRouter();
+  const pathname = usePathname();
+  const setParams = useParamsStore((state) => state.setParams);
+
+  function setWinner() {
+    setParams({ winner: user.username, seller: undefined });
+
+    if (pathname !== "/") router.push("/");
+  }
+
+  function setSeller() {
+    setParams({ seller: user.username, winner: undefined });
+
+    if (pathname !== "/") router.push("/");
+  }
 
   return (
     <Dropdown inline label={`Welcome ${user.name}`}>
-      <DropdownItem icon={HiUser}>
-        <Link href="/">My Auctions</Link>
+      <DropdownItem icon={HiUser} onClick={setSeller}>
+        My Auctions
       </DropdownItem>
-      <DropdownItem icon={AiFillTrophy}>
-        <Link href="/">Auctions won</Link>
+      <DropdownItem icon={AiFillTrophy} onClick={setWinner}>
+        Auctions won
       </DropdownItem>
       <DropdownItem icon={AiFillCar}>
         <Link href="/auctions/create">Sell my car</Link>
--- frontend/web-app/types/next-auth.d.ts
+++ frontend/web-app/types/next-auth.d.ts
@@ -15,6 +15,10 @@ declare module "next-auth" {
   interface Profile {
     username: string;
   }
+
+  interface User {
+    username: string;
+  }
 }
 
 declare module "next-auth/jwt" {
