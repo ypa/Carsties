46a0a1d7bf97bf61711fadefe9ac56f24a360350
Yan Aung <yaung@servicetitan.com>
2024-09-21 19:58:46 -0700

    Add http fetchWrapper and refactor

--- frontend/web-app/app/actions/auctionActions.ts
+++ frontend/web-app/app/actions/auctionActions.ts
@@ -1,14 +1,11 @@
 "use server";
 
 import { auth } from "@/auth";
+import { fetchWrapper } from "@/lib/fetchWrapper";
 import { Auction, PagedResult } from "@/types";
 
 export async function getData(query: string): Promise<PagedResult<Auction>> {
-  const res = await fetch(`http://localhost:6001/search${query}`);
-
-  if (!res.ok) throw new Error("Failed to fetch data");
-
-  return res.json();
+  return await fetchWrapper.get(`search${query}`);
 }
 
 export async function updateAuctionTest() {
