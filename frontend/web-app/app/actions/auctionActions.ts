6a0bcb2dc02bb29bb4f9d199259cd1588998e3d4
Yan Aung <yaung@servicetitan.com>
2024-09-24 19:16:56 -0700

    Add delete functionality

--- frontend/web-app/app/actions/auctionActions.ts
+++ frontend/web-app/app/actions/auctionActions.ts
@@ -34,3 +34,7 @@ export async function updateAuction(id: string, data: FieldValues) {
   revalidatePath(`/auctions/${id}`);
   return res;
 }
+
+export async function deleteAuction(id: string) {
+  return await fetchWrapper.del(`auctions/${id}`);
+}
