b9a89bff2e82621b4fa000e22227c70d92448de9
Yan Aung <yaung@servicetitan.com>
2024-09-30 18:59:48 -0700

    Publish BidPlaced event

--- src/BiddingService/Controllers/BidsController.cs
+++ src/BiddingService/Controllers/BidsController.cs
@@ -2,6 +2,8 @@ using System;
 using AutoMapper;
 using BiddingService.DTOs;
 using BiddingService.Models;
+using Contracts;
+using MassTransit;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using MongoDB.Entities;
@@ -14,10 +16,12 @@ public class BidsController : ControllerBase
 
 {
     private readonly IMapper _mapper;
+    private readonly IPublishEndpoint _publishEndpoint;
 
-    public BidsController(IMapper mapper)
+    public BidsController(IMapper mapper, IPublishEndpoint publishEndpoint)
     {
         _mapper = mapper;
+        _publishEndpoint = publishEndpoint;
     }
 
     [Authorize]
@@ -69,6 +73,8 @@ public class BidsController : ControllerBase
         }
 
         await DB.SaveAsync(bid);
+
+        await _publishEndpoint.Publish(_mapper.Map<BidPlaced>(bid));
 
         return Ok(_mapper.Map<BidDto>(bid));
     }
