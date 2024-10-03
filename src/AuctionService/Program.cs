45127157216fe88d4f625721f91d3205621b3a25
Yan Aung <yaung@servicetitan.com>
2024-10-02 19:37:05 -0700

    Add GrpcAuctionService

--- src/AuctionService/Program.cs
+++ src/AuctionService/Program.cs
@@ -1,5 +1,6 @@
 using AuctionService;
 using AuctionService.Data;
+using AuctionService.Services;
 using MassTransit;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
@@ -48,6 +49,7 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
     });
 
 builder.Services.AddScoped<IAuctionRepository, AuctionRepository>();
+builder.Services.AddGrpc();
 
 var app = builder.Build();
 
@@ -56,6 +58,7 @@ app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
+app.MapGrpcService<GrpcAuctionService>();
 
 try
 {
---
+++ src/AuctionService/Services/GrpcAuctionService.cs
@@ -0,0 +1,36 @@
+using System;
+using AuctionService.Data;
+using Grpc.Core;
+
+namespace AuctionService.Services;
+
+public class GrpcAuctionService : GrpcAuction.GrpcAuctionBase // interit from class generated automatically
+{
+    private readonly AuctionDbContext _dbContext;
+
+    public GrpcAuctionService(AuctionDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    public override async Task<GrpcAuctionResponse> GetAuction(GetAuctionRequest request, ServerCallContext context)
+    {
+        Console.WriteLine("==> Received Grpc Request for auction");
+
+        var auction = await _dbContext.Auctions.FindAsync(Guid.Parse(request.Id))
+            ?? throw new RpcException(new Status(StatusCode.NotFound, "Not found"));
+
+        var response = new GrpcAuctionResponse
+        {
+            Auction = new GrpcAuctionModel
+            {
+                AuctionEnd = auction.AuctionEnd.ToString(),
+                Id = auction.Id.ToString(),
+                ReservePrice = auction.ReservePrice,
+                Seller = auction.Seller
+            }
+        };
+
+        return response;
+    }
+}
