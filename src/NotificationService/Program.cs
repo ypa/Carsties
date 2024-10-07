b98244190bba226f273939830f7e33e62dc4337d
Yan Aung <yaung@servicetitan.com>
2024-10-06 19:38:05 -0700

    Configure RabbitMq/MassTransit

--- src/NotificationService/Program.cs
+++ src/NotificationService/Program.cs
@@ -1,4 +1,23 @@
+using MassTransit;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddMassTransit(x =>
+{
+    x.SetEndpointNameFormatter(new KebabCaseEndpointNameFormatter("nt", false));
+
+    x.UsingRabbitMq((context, cfg) =>
+    {
+        cfg.Host(builder.Configuration["RabbitMq:host"], "/", host =>
+        {
+            host.Username(builder.Configuration.GetValue("RabbitMq:Username", "guest"));
+            host.Password(builder.Configuration.GetValue("RabbitMq:Password", "guest"));
+        });
+
+        cfg.ConfigureEndpoints(context);
+    });
+});
+
 var app = builder.Build();
 
 app.Run();
