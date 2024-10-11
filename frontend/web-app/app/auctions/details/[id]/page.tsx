debb71595614c48ba8041b6d09758330c76fc226
Yan Aung <yaung@servicetitan.com>
2024-10-10 19:24:59 -0700

    Refactor as BidList component

--- frontend/web-app/app/auctions/details/[id]/page.tsx
+++ frontend/web-app/app/auctions/details/[id]/page.tsx
@@ -11,11 +11,11 @@ import { getCurrentUser } from "@/app/actions/authActions";
 import EditButton from "./EditButton";
 import DeleteButton from "./DeleteButton";
 import BidItem from "./BidItem";
+import BidList from "./BidList";
 
 export default async function Details({ params }: { params: { id: string } }) {
   const data = await getDetailedViewData(params.id);
   const user = await getCurrentUser();
-  const bids = await getBidsForAuction(params.id);
 
   return (
     <div>
@@ -39,12 +39,8 @@ export default async function Details({ params }: { params: { id: string } }) {
         <div className="w-full bg-gray-200 relative aspect-[4/3] aspect-w-16 rounded-lg overflow-hidden">
           <CarImage imageUrl={data.imageUrl} />
         </div>
-        <div className="border-2 rounded-lg p-2 bg-gray-100">
-          <Heading title="Bids" subtitle="Bids" />
-          {bids.map((bid) => (
-            <BidItem key={bid.id} bid={bid} />
-          ))}
-        </div>
+
+        <BidList user={user} auction={data} />
       </div>
 
       <div className="mt-3 grid grid-cols-1 rounded-lg">
