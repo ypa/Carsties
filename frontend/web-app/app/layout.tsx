c2f492a5d906b9d3bfe412e182a4267cae734483
Yan Aung <yaung@servicetitan.com>
2024-10-14 19:05:04 -0700

    Refactor hardcoded localhost URLs

--- frontend/web-app/app/layout.tsx
+++ frontend/web-app/app/layout.tsx
@@ -16,13 +16,17 @@ export default async function RootLayout({
   children: React.ReactNode;
 }>) {
   const user = await getCurrentUser();
+  const notifyUrl = process.env.NOTIFY_URL;
+
   return (
     <html lang="en">
       <body>
         <ToasterProvider />
         <Navbar />
         <main className="container mx-auto px-5 pt-10">
-          <SignalRProvider user={user}>{children}</SignalRProvider>
+          <SignalRProvider user={user} notifyUrl={notifyUrl!}>
+            {children}
+          </SignalRProvider>
         </main>
       </body>
     </html>
--- frontend/web-app/auth.ts
+++ frontend/web-app/auth.ts
@@ -11,7 +11,7 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
       id: "id-server",
       clientId: "nextApp",
       clientSecret: "secret",
-      issuer: "http://localhost:5000",
+      issuer: process.env.ID_URL,
       authorization: { params: { scope: "openid profile auctionApp" } },
       idToken: true,
     } as OIDCConfig<Omit<Profile, "username">>),
